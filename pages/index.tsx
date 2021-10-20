94944b8791d6b3183c94775432336f3a0f035c51
Lee-sungheon <leesungheon.dev@gmail.com>
2021-10-21 00:34:58 +0900

    ci(dev): serverless 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
-  background: yellow;
+  background-color: yellow;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const Home = (): JSX.Element => {

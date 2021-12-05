069befd096ae103ba058988cfc67829b6d7da0e4
ClarXo <imp_rove@naver.com>
2021-12-05 23:51:37 +0900

    Feat/feed detail (#23)
    * feat : feed detail page
    
    * feat(bottom bar): make bottom bar (#17)
    
    커뮤니티 리스트에 하단 바를 만들었습니다.
    
    Co-authored-by: hyemingway <hyemingway@baghyemin-ui-MacBook-Pro.local>
    
    * Revert "feat(bottom bar): make bottom bar (#17)" (#18)
    
    This reverts commit c5c3afef63a1cfe8a8fe599f1e1b9c429939bca5.
    
    * fix(detailPost): pr 피드백 적용
    
    * style(detailPost, postItem): overflow 설정 변경
    
    * fix(detailPost): router pathname 변경
    
    * style(detailPost): 이미지 너비 변경
    
    * fix: eslint 오류 수정 및 router 코드 정정
    
    Co-authored-by: hyemingway2 <53967837+hyemingway2@users.noreply.github.com>
    Co-authored-by: hyemingway <hyemingway@baghyemin-ui-MacBook-Pro.local>
    Co-authored-by: 이성헌 <rkd028@naver.com>

---
+++ pages/community/detail.tsx
@@ -0,0 +1,6 @@
+import { DetailPostTemplate } from '@/components/templates';
+import React from 'react';
+
+export default function DetailPostPage(): JSX.Element {
+  return <DetailPostTemplate />;
+}
